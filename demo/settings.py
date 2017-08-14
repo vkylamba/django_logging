bd4f6913758da28757cbc3af4ff9e587fcf4e2d1
Joost VanDorp <joostvandorp@gmail.com>
2017-08-14 15:48:08 -0400

    Add test directory for travis ci

---
+++ demo/settings.py
@@ -0,0 +1,72 @@
+import os
+import uuid
+
+gettext = lambda s: s  # noqa
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(os.path.dirname(__file__), 'demo.db')
+    }
+}
+
+TIME_ZONE = 'UTC'
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+SECRET_KEY = str(uuid.uuid4())
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'demo.urls'
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'django.contrib.staticfiles',
+    'django_logging',
+)
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+USE_TZ = True
+USE_I18N = True
+USE_L10N = True
+
+LANGUAGE_CODE = 'en'
+
+LOG_ENV_NAME = 'test'
+LOG_DIR = '/tmp'
+LOG_FILE_NAME = 'test.log'
+APPLICATION_LOG_LEVEL = 'INFO'
