67b62ca6211d5730e4de1da65d66946bde67fd12
Syed Is Saqlain <saqlain.syed007@gmail.com>
2018-04-30 15:56:44 +0530

    Manually Type Cast to int

--- django_logging/queue_listner.py
+++ django_logging/queue_listner.py
@@ -7,7 +7,7 @@ environ.Env.read_env()
 log_queue_size = env('LOG_HANDLER_QUEUE_SIZE', default=10000)
 
 # Log queue used by queue handler
-log_queue = Queue(maxsize=log_queue_size)
+log_queue = Queue(maxsize=int(log_queue_size))
 
 queue_listner = QueueListener(
     log_queue
